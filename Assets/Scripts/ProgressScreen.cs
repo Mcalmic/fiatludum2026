cd21d76da1074747544e7db8349e8f6a8c5f6acd
Mcalmic <themcalmic@gmail.com>
2026-04-13 17:11:15 -0700

    progress bar

--- Assets/Scripts/ProgressScreen.cs
+++ Assets/Scripts/ProgressScreen.cs
@@ -1,20 +1,54 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class ProgressScreen : MonoBehaviour
 {
-    GameManager gameManager;
     [SerializeField] TextMeshProUGUI distanceText;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] TextMeshProUGUI timeElapsedText;
+
+    [Header("Progress Bar")]
+    [SerializeField] private RectTransform barTrack;
+    [SerializeField] private Image progressFill;
+    [SerializeField] private RectTransform shipIcon;
+
+    private float timeElapsed = 0f;
+    private float startDistance = -1f;
+
     void Start()
     {
-        gameManager = GameManager.instance;
+        if (GameProgressManager.instance != null)
+            startDistance = GameProgressManager.instance.GetDistanceToDestination();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        distanceText.text = "Distance to Earth: " + Mathf.CeilToInt(gameManager.distanceLeft) + " km";
+        timeElapsed += Time.deltaTime;
+
+        int minutes = Mathf.FloorToInt(timeElapsed / 60f);
+        int seconds = Mathf.FloorToInt(timeElapsed % 60f);
+        timeElapsedText.text = "Time Elapsed: " + minutes.ToString("00") + ":" + seconds.ToString("00");
+
+        float dist = GameProgressManager.instance != null ? GameProgressManager.instance.GetDistanceToDestination() : 0f;
+        distanceText.text = "Distance to Destination: " + Mathf.CeilToInt(dist) + " km";
+
+        UpdateProgressBar(dist);
+    }
+
+    private void UpdateProgressBar(float currentDist)
+    {
+        if (startDistance <= 0f) return;
+
+        float progress = Mathf.Clamp01(1f - (currentDist / startDistance));
+
+        if (progressFill != null)
+            progressFill.fillAmount = progress;
+
+        if (shipIcon != null && barTrack != null)
+        {
+            float minX = barTrack.rect.xMin;
+            float maxX = barTrack.rect.xMax;
+            shipIcon.anchoredPosition = new Vector2(Mathf.Lerp(minX, maxX, progress), shipIcon.anchoredPosition.y);
+        }
     }
 }
